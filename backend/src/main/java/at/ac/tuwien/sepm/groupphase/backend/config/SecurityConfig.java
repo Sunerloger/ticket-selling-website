39d6caccc467f5a01f3e703b374a522c82785dcf
Pucci345 <KissenschlachtLP@gmail.com>
2023-04-26 15:54:00 +0200

    Enabled h2-Console in browser, added schema creation file

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/config/SecurityConfig.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
         http.cors().and()
             .csrf().disable()
             .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
