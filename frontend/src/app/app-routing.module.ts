bf38a3576b387f14d1a1a1ccbb8d671c4b3f0bb2
Elias Anton Rozanics <e12122077@student.tuwien.ac.at>
2023-05-16 01:08:01 +0200

    [Hotfix] Prevented non-admins from accessing creation of events per route.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import {EventsComponent} from './components/events/events.component';
 import {RegisterComponent} from './components/register/register.component';
 import {NewsCreateComponent} from './components/news/news-create/news-create.component';
 import {NewsComponent} from './components/news/news.component';
-import {RouteGuard} from './guards/route.guard';
+import {AdminRouteGuard} from './guards/admin-route.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -16,9 +16,9 @@ const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'message', canActivate: [AuthGuard], component: MessageComponent},
   {path: 'message', canActivate: [AuthGuard], component: MessageComponent},
-  {path: 'events', component: EventsComponent},
+  {path: 'events', canActivate: [AdminRouteGuard], component: EventsComponent},
   {path: 'news', canActivate: [AuthGuard], component: NewsComponent},
-  {path: 'news/create', canActivate: [RouteGuard], component: NewsCreateComponent}
+  {path: 'news/create', canActivate: [AdminRouteGuard], component: NewsCreateComponent}
 ];
 
 @NgModule({
