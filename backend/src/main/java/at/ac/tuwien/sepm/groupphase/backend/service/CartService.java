a1ed0d5a5ba3231084b137783017095602031e68
Jakob Kästner <e1626126@student.tuwien.ac.at>
2023-05-30 15:15:10 +0200

    added javadoc to service interfaces
    Also removed unused imports in some methods

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/CartService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/CartService.java
@@ -1,17 +1,34 @@
 package at.ac.tuwien.sepm.groupphase.backend.service;
 
-
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.CartItemDto;
-import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.hallplan.HallPlanSeatDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.SeatDto;
 import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 
 import java.util.List;
 
 public interface CartService {
+    /**
+     * Adds a List of items to the Cart of the given user
+     *
+     * @param seatDtoList the list of Seats to add to the cart
+     * @param userId      the id of the user
+     * @throws NotFoundException if one of the seats doesn't exist
+     */
     void addItemList(List<SeatDto> seatDtoList, Long userId) throws NotFoundException;
 
-    public List<CartItemDto> getItems(Long userId);
+    /**
+     * Fetches a list of all the items in the given users cart
+     *
+     * @param userId the id of the user
+     * @return a List of CartItemDtos of the given user
+     */
+    List<CartItemDto> getItems(Long userId);
 
+    /**
+     * removes a seat from the cart of the given user
+     *
+     * @param itemId the id of the to delete seat
+     * @param userId the id of the user
+     */
     void deleteItem(Long itemId, Long userId);
 }
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/ReservationService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/ReservationService.java
@@ -1,23 +1,47 @@
 package at.ac.tuwien.sepm.groupphase.backend.service;
 
-import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.CartItemDto;
-import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.hallplan.HallPlanSeatDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.ReservationDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.SeatDto;
-import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 public interface ReservationService {
 
-
+    /**
+     * Fetches all the Reservations of a given user
+     *
+     * @param userId the id of the user
+     * @return a List of ReservationDtos
+     */
     List<ReservationDto> getReservationsOfUser(Long userId);
 
+    /**
+     * Fetches the Reservation with the given reservationNr.
+     * Will only return the reservation if the reservation belongs to the given user.
+     *
+     * @param reservationNr the id of the reservation to fetch
+     * @param userId the id of the user that tries to fetch the reservation
+     * @return the requested Reservation
+     */
     ReservationDto getReservationOfUser(Long reservationNr, Long userId);
 
+    /**
+     * Deletes the Reservation with the given reservationNr.
+     * Will only delete the reservation if the reservation belongs to the given user.
+     *
+     * @param reservationNr the id of the reservation to fetch
+     * @param userId the id of the user that tries to fetch the reservation
+     */
     @Transactional
     void deleteReservation(Long reservationNr, Long userId);
 
+    /**
+     * Reserves the given items. (sets the status of the seats to RESERVED)
+     * Items will only get added if they are FREE.
+     *
+     * @param itemDtoList the list of items to be added
+     * @param userId the id of the user tries to delete the reservation
+     */
     void addReservation(List<SeatDto> itemDtoList, Long userId);
 }
