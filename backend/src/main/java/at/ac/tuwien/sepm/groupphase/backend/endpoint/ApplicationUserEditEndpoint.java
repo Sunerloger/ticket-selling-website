6ad5c0feac06eb7c656d1bfc1afb4dca33d83b02
Martin Gerdenich <e11776233@student.tuwien.ac.at>
2023-05-31 11:30:19 +0200

    Changed Token aquisition
    Token is now read from authorization header and not as an request param

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ApplicationUserEditEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ApplicationUserEditEndpoint.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -61,7 +62,7 @@ public class ApplicationUserEditEndpoint {
 
     @GetMapping
     @PermitAll
-    public UserDetailDto getUser(@RequestParam(value = "token") String token) {
+    public UserDetailDto getUser(@RequestHeader("Authorization") String token) {
         LOGGER.info("GET USER " + BASE_PATH + "with TOKEN" + token);
         return userMapper.entityToUserDetailDto(userService.getUser(token));
     }
