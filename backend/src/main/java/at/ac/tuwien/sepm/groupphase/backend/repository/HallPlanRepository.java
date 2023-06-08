981e6e0e5f06a35b9bd0d97e2b39f84a3e1287b9
Leonhard Kenshi Müllauer <52050699+Reonharudo@users.noreply.github.com>
2023-06-08 15:06:32 +0200

    fix #69

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/HallPlanRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/HallPlanRepository.java
@@ -36,7 +36,7 @@ public interface HallPlanRepository extends JpaRepository<HallPlan, Long> {
     @Query("SELECT s.section FROM HallPlan hp JOIN hp.seatRows sr JOIN sr.seats s WHERE hp.id = :hallPlanId")
     List<HallPlanSection> findAllSectionsByHallPlanId(@Param("hallPlanId") Long hallPlanId);
 
-    @Query("SELECT s.section, COUNT(s.section) AS count FROM HallPlan hp JOIN hp.seatRows sr JOIN sr.seats s JOIN s.section WHERE hp.id = :hallPlanId AND sr.hallPlanId = :hallPlanId GROUP BY s.section.id")
+    @Query("SELECT s.section, COUNT(s.section) AS count FROM HallPlan hp JOIN hp.seatRows sr JOIN sr.seats s JOIN s.section WHERE hp.id = :hallPlanId AND s.type <> 'VACANT_SEAT' AND sr.hallPlanId = :hallPlanId GROUP BY s.section.id")
     List<Object[]> findAllSectionsByHallPlanIdCounts(@Param("hallPlanId") Long hallPlanId);
 
     @Query("SELECT sh, 0 AS count FROM HallPlanSection sh WHERE sh.hallPlanId = :hallPlanId")
