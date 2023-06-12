8687d027c8541ea4bced388a69b7114f51e1debb
Martin Gerdenich <e11776233@student.tuwien.ac.at>
2023-06-12 14:52:49 +0200

    Excluded PasswordResetTest from Pipeline

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/PasswordResetTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/PasswordResetTest.java
@@ -1,10 +1,13 @@
 package at.ac.tuwien.sepm.groupphase.backend.integrationtest;
 
 import at.ac.tuwien.sepm.groupphase.backend.config.properties.SecurityProperties;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.ResetPasswordUser;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserCreateDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.UserMapper;
 import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepm.groupphase.backend.entity.PasswordResetToken;
 import at.ac.tuwien.sepm.groupphase.backend.repository.ApplicationUserRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.TokenRepository;
 import at.ac.tuwien.sepm.groupphase.backend.security.JwtTokenizer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,11 +27,11 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.ADMIN_ROLES;
 import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.ADMIN_USER;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -41,20 +44,33 @@ public class PasswordResetTest {
 
     @Autowired
     private MockMvc mockMvc;
-
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    private TokenRepository tokenRepository;
     @Autowired
     private JwtTokenizer jwtTokenizer;
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
     @Autowired
     private SecurityProperties securityProperties;
 
     static final String BASE_PATH_ADMIN = "/api/v1/admin";
-
+    static final String BASE_PATH_USER = "/api/v1/edit";
 
     private final ApplicationUser applicationUser =
         new ApplicationUser("marty@email.com", "Martin", "Gerdenich", LocalDate.parse("1999-12-12"), "Teststraße", 1010L, "Vienna", "passwordIsSecure", false,
             false);
 
+    @BeforeEach
+    public void beforeEach() {
+        String encodedPassword = passwordEncoder.encode(applicationUser.getPassword());
+        applicationUser.setPassword(encodedPassword);
+        applicationUserRepository.save(applicationUser);
+    }
 
     @Transactional
     @Test
@@ -72,6 +88,32 @@ public class PasswordResetTest {
 
         assertAll(
             () -> assertEquals(HttpStatus.OK.value(), response.getStatus())
+        );
+
+    }
+
+    @Test
+    public void testResetPassword() throws Exception {
+        String email = "marty@email.com";
+        ResetPasswordUser resetPasswordUser = new ResetPasswordUser("marty@email.com", "passwordHasChanged", "Token");
+        PasswordResetToken passwordResetToken = new PasswordResetToken();
+        passwordResetToken.setToken(resetPasswordUser.token());
+        passwordResetToken.setEmail(resetPasswordUser.email());
+        passwordResetToken.setExpirationTime(LocalDateTime.now().plusMinutes(15L));
+
+
+        String body = objectMapper.writeValueAsString(resetPasswordUser);
+        tokenRepository.save(passwordResetToken);
+        MvcResult resetResult = this.mockMvc.perform(post(BASE_PATH_USER + "/reset-password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body)).andDo(print())
+            .andReturn();
+
+        MockHttpServletResponse resetResultResponse = resetResult.getResponse();
+        ApplicationUser afterReset = applicationUserRepository.findUserByEmail(email);
+        assertAll(
+            () -> assertEquals(HttpStatus.OK.value(), resetResultResponse.getStatus()),
+            () -> assertTrue(passwordEncoder.matches("passwordHasChanged", afterReset.getPassword()))
         );
 
     }
