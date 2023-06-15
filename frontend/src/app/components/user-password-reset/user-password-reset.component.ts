59c18f218e92cbbffb9ad05b41f54e79f54c4624
Martin Gerdenich <e11776233@student.tuwien.ac.at>
2023-06-15 13:00:43 +0200

    Fixed Checkstyle Violation

--- frontend/src/app/components/user-password-reset/user-password-reset.component.ts
+++ frontend/src/app/components/user-password-reset/user-password-reset.component.ts
@@ -3,7 +3,6 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../services/user.service';
 import {AuthService} from '../../services/auth.service';
 import {ToastrService} from 'ngx-toastr';
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-password-reset',
