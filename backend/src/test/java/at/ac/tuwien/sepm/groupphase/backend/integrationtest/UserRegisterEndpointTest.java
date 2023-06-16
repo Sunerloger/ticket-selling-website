8c326f00bc0cfeba717173a23c1aeb2859024a34
Martin Gerdenich <e11776233@student.tuwien.ac.at>
2023-06-16 11:03:44 +0200

    Added Tests to test passwordRequirements

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/UserRegisterEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/UserRegisterEndpointTest.java
@@ -28,6 +28,7 @@ import java.time.LocalDate;
 import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.ADMIN_ROLES;
 import static at.ac.tuwien.sepm.groupphase.backend.basetest.TestData.ADMIN_USER;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -175,4 +176,43 @@ public class UserRegisterEndpointTest {
             .andExpect(jsonPath("$.errors[*].message",
                 containsInAnyOrder("First name must contain only letters", "Last name must contain only letters")));
     }
+
+    @Transactional
+    @Test
+    public void givenUserRegisterDtoWithWrongPasswordRequirements_whenRegisterUser_ValidationFails() throws Exception {
+        UserRegisterDto userRegisterDto = new UserRegisterDto(
+            -1000L, "john@example.com", "John", "Example", LocalDate.parse("1988-12-12"),
+            "Teststreet", 1010L, "Vienna", "PasswordIsWeak", false, false
+        );
+        String requestBody = objectMapper.writeValueAsString(userRegisterDto);
+
+        mockMvc.perform(post("/api/v1/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(jsonPath("$.errors[0].message").value(containsString("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")));
+    }
+
+    @Transactional
+    @Test
+    public void givenUserRegisterDtoWithCorrectPasswordRequirements_whenRegisterUser_UserIsRegistered() throws Exception {
+        UserRegisterDto userRegisterDto = new UserRegisterDto(
+            -1000L, "john@example.com", "John", "Example", LocalDate.parse("1988-12-12"),
+            "Teststreet", 1010L, "Vienna", "Password123%", false, false
+        );
+        String requestBody = objectMapper.writeValueAsString(userRegisterDto);
+
+        mockMvc.perform(post("/api/v1/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isCreated());
+
+        ApplicationUser registeredUser = applicationUserRepository.findUserByEmail("john@example.com");
+        assertNotNull(registeredUser);
+        assertEquals("John", registeredUser.getFirstName());
+        assertEquals("Example", registeredUser.getLastName());
+        assertEquals(LocalDate.parse("1988-12-12"), registeredUser.getBirthdate());
+
+    }
+
 }
