bb62bbe56e3249160edecdc71585aa7e35d6609c
Daniel Vercimak <e12023989@student.tuwien.ac.at>
2023-06-19 11:50:08 +0200

    Columns now clickable and redirect to event-detail

--- frontend/src/app/components/event-chart/event-chart.component.ts
+++ frontend/src/app/components/event-chart/event-chart.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Event} from '../../dtos/event';
 import {EventService} from '../../services/event.service';
 import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-event-chart',
@@ -10,20 +11,28 @@ import {ToastrService} from 'ngx-toastr';
 })
 export class EventChartComponent implements OnInit{
   chart: any;
+  goToEvent= (e) =>  {
+    this.router.navigateByUrl('/event/'+e.dataPoint.id);
+  };
+  // eslint-disable-next-line @typescript-eslint/member-ordering
   chartOptions = {
     title: {
+      fontFamily: '"Segoe UI",Roboto',
       text: 'Top 10 Events'
     },
     data: [{
+      click: this.goToEvent,
       type: 'column',
       dataPoints: [
 
       ]
     }]
   };
+  // eslint-disable-next-line @typescript-eslint/member-ordering
   constructor(
     private eventService: EventService,
-    private notification: ToastrService
+    private notification: ToastrService,
+    private router: Router
   ) { }
   ngOnInit(): void {
   this.fetchTopEvents();
@@ -40,7 +49,7 @@ export class EventChartComponent implements OnInit{
           let count = 0;
           for (const event of data) {
             if(event.soldTickets > 0){
-              this.addDataPoint(event.title, count, event.soldTickets);
+              this.addDataPoint(event.title, count, event.soldTickets, event.id);
               count++;
             }
           }
@@ -56,8 +65,10 @@ export class EventChartComponent implements OnInit{
     });
   }
 
-  addDataPoint(label: string, count: number, value: number): void {
-    const newDataPoint = { label,x: count, y: value };
+  addDataPoint(label: string, count: number, value: number, id: number): void {
+    const newDataPoint = { label,x: count, y: value, id };
     this.chartOptions.data[0].dataPoints.push(newDataPoint);
   }
+
+
 }
