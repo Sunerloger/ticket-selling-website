58accb1c5ca25ac562f05a171e11edfccd1805f7
Lorenz Müllauer <e12024710@student.tuwien.ac.at>
2023-06-19 15:32:28 +0000

    Revert "Merge branch '51-3-1-pdf-print-of-tickets' into 'main'"
    This reverts merge request !95

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TicketValidatorServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/TicketValidatorServiceImpl.java
@@ -2,8 +2,6 @@ package at.ac.tuwien.sepm.groupphase.backend.service.impl;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.TicketDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.TicketPayloadDto;
-import at.ac.tuwien.sepm.groupphase.backend.entity.Ticket;
-import at.ac.tuwien.sepm.groupphase.backend.repository.TicketRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.TicketValidatorService;
 import jakarta.xml.bind.ValidationException;
 import org.slf4j.Logger;
@@ -30,116 +28,71 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Base64;
-import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class TicketValidatorServiceImpl implements TicketValidatorService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private static final String filePath = "./src/main/resources/KeyFiles.txt";
-
-    private final TicketRepository ticketRepository;
-
-    private static final String TICKET_INVALID = "Ticket is invalid!";
-
-    private static final String TICKET_VALID = "Ticket is valid!";
-
-    public TicketValidatorServiceImpl(TicketRepository ticketRepository) {
-        this.ticketRepository = ticketRepository;
+    public TicketValidatorServiceImpl() {
 
     }
 
     @Override
     public TicketPayloadDto getTicketPayload(TicketDto ticketDto)
         throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, ValidationException {
+        ArrayList<SecretKey> secretKeys = new ArrayList<>();
         if (ticketDto == null || ticketDto.getEvent() == null || ticketDto.getSeat() == null) {
             throw new jakarta.validation.ValidationException("The parameters Event and Seat of the Ticket must be provided");
         }
-        return generatePayload(ticketDto);
+        return generatePayload("./src/main/resources/KeyFiles.txt", ticketDto);
+        //writeKeysToFile("./src/main/resources/KeyFiles.txt", secretKeys);
     }
 
-    // ! Only call to set initial key values !
-    private void writeKeysToFile(ArrayList<SecretKey> secretKeys) {
-        // Generate a random initialization vector (IV)
-        byte[] iv = new byte[16];
-
-        SecureRandom randomIv = new SecureRandom();
-        randomIv.nextBytes(iv);
-
+    private void writeKeysToFile(String filePath, ArrayList<SecretKey> secretKeys) {
         //Write 32 Byte Key Array to File encode as Base 64 String
         byte[] keyBytes = new byte[32];
         SecureRandom random = new SecureRandom();
         random.nextBytes(keyBytes);
-        String encodedKey = encode(keyBytes);
-        String encodedIv = encode(iv);
+        String encoded = encode(keyBytes);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            writer.write(encodedKey);
-            writer.newLine();
-            writer.write(encodedIv);
+            writer.write(encoded);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public TicketPayloadDto validatePayload(TicketPayloadDto payloadDto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
-        //Create TicketPayloadDto
-        TicketPayloadDto newPayload = new TicketPayloadDto();
-        //Retrieve KeyFileSpecs
-        KeyFileSpecs specs = readKeyFileSpecs();
-        //Decrypt Message with retrieved KeySpecs
-        String decryptedMsg = "";
-        try {
-            decryptedMsg = aesDecrypt(payloadDto.getMessage(), specs.getSecretKey(), specs.getIvParameterSpec());
-        } catch (IllegalArgumentException ex) {
-            newPayload.setMessage(TICKET_INVALID);
-            return newPayload;
-        }
-
-        String[] msgComponents = decryptedMsg.split(" ");
-        if (msgComponents.length != 4) {
-            newPayload.setMessage(TICKET_INVALID);
-            return newPayload;
-        }
-        Long ticketId = Long.valueOf(msgComponents[0]);
-        Long seatId = Long.valueOf(msgComponents[1]);
-        //Retrieve ticket
-        Optional<Ticket> ticket = ticketRepository.findById(ticketId);
-        if (!ticket.isPresent()) {
-            newPayload.setMessage(TICKET_INVALID);
-        } else {
-            if (Objects.equals(ticket.get().getSeatId(), seatId)) {
-                newPayload.setMessage(TICKET_VALID);
-            } else {
-                newPayload.setMessage(TICKET_INVALID);
-            }
-        }
-        try {
-            if (!Long.valueOf(msgComponents[3]).equals(Long.valueOf("5839593258"))) {
-                newPayload.setMessage(TICKET_INVALID);
-            }
-        } catch (NumberFormatException ex) {
-            newPayload.setMessage(TICKET_INVALID);
-        }
-        return newPayload;
-    }
-
-    private TicketPayloadDto generatePayload(TicketDto ticketDto)
+    private TicketPayloadDto generatePayload(String filePath, TicketDto ticketDto)
         throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        // Generate a random initialization vector (IV)
+        byte[] iv = new byte[16];
 
-        if (ticketDto.getTicketNr() == null) {
-            throw new jakarta.validation.ValidationException("Ticket Nr must be provided");
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        String keyBytes;
+
+        //Retrieve Key Bytes from file
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            keyBytes = reader.readLine();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        //Retrieve KeyFileSpecs
-        KeyFileSpecs specs = readKeyFileSpecs();
+        // Create a SecretKey object using the provided key bytes
+        SecretKey secretKey = new SecretKeySpec(decode(keyBytes), "AES");
 
         //Encrypt message
         String payloadMsg = "";
 
-        payloadMsg += ticketDto.getTicketNr() + " " + ticketDto.getSeat().getId() + " " + ticketDto.getSeat().getSeatNr() + " 5839593258";
-        String encryptedMessage = aesEncrypt(payloadMsg, specs.getSecretKey(), specs.getIvParameterSpec());
+        if (ticketDto.getTicketNr() == null) {
+            throw new jakarta.validation.ValidationException("Ticket Nr must be provided");
+        }
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        payloadMsg += ticketDto.getTicketNr() + " " + ticketDto.getSeat().getId() + " " + ticketDto.getEvent().getTitle() + " 5839593258";
+        String encryptedMessage = aesEncrypt(payloadMsg, secretKey, ivParameterSpec);
         //String decryptedMessage = aesDecrypt(encryptedMessage, secretKey, ivParameterSpec);
         TicketPayloadDto ticketPayloadDto = new TicketPayloadDto();
         ticketPayloadDto.setMessage(encryptedMessage);
@@ -162,49 +115,11 @@ public class TicketValidatorServiceImpl implements TicketValidatorService {
         return new String(cipherText);
     }
 
-    public KeyFileSpecs readKeyFileSpecs() {
-        String keyBytes;
-        String ivBytes;
-
-        // Retrieve Key Bytes from file
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            keyBytes = reader.readLine();
-            ivBytes = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Create a SecretKey object using the provided key bytes
-        SecretKey secretKey = new SecretKeySpec(decode(keyBytes), "AES");
-
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(decode(ivBytes));
-
-        return new KeyFileSpecs(secretKey, ivParameterSpec);
-    }
-
     private static String encode(byte[] data) {
         return Base64.getEncoder().encodeToString(data);
     }
 
     private static byte[] decode(String data) {
         return Base64.getDecoder().decode(data);
-    }
-
-    public static class KeyFileSpecs {
-        private SecretKey secretKey;
-        private IvParameterSpec ivParameterSpec;
-
-        public KeyFileSpecs(SecretKey secretKey, IvParameterSpec ivParameterSpec) {
-            this.secretKey = secretKey;
-            this.ivParameterSpec = ivParameterSpec;
-        }
-
-        public SecretKey getSecretKey() {
-            return secretKey;
-        }
-
-        public IvParameterSpec getIvParameterSpec() {
-            return ivParameterSpec;
-        }
     }
 }
